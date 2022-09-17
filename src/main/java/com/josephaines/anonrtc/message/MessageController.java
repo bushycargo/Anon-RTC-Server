22cc4687921be619b64cd9a149c56f1c0ca2b1b7
bushycargo <josephaines03@gmail.com>
2022-09-17 04:10:52 +0100

    MessageController.java created

---
+++ src/main/java/com/josephaines/anonrtc/message/MessageController.java
@@ -0,0 +1,26 @@
+package com.josephaines.anonrtc.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path ="api/v1/message")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public List<Message> getMessages(){
+        return messageService.getMessages();
+    }
+
+}
