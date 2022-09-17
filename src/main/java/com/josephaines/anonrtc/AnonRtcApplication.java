a138486a727d61bd45be46479a5a80dbee4515da
bushycargo <josephaines03@gmail.com>
2022-09-17 03:47:46 +0100

    Initial Commit

---
+++ src/main/java/com/josephaines/anonrtc/AnonRtcApplication.java
@@ -0,0 +1,13 @@
+package com.josephaines.anonrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonRtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonRtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josephaines/anonrtc/AnonRtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephaines.anonrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonRtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
